424207b4bfba6d56cbc38d94003c2396a751917c
Neil Berg <neil.berg14@gmail.com>
2019-07-07 15:04:06 -0400

    disasble scroll on menu open

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,29 +1,17 @@
 import React from "react"
 import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
 
 import Sidebar from "./Sidebar"
 import "./layout.css"
 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
-
   return (
     <>
-      {/* <Sidebar /> */}
+      <Sidebar />
       <main>{children}</main>
       <footer>
-        © {new Date().getFullYear()}, Built with
-        {` `}
-        <a href="https://www.gatsbyjs.org">Gatsby</a>
+        site by
+        <a href="https://neilberg.dev">neil berg</a>
       </footer>
     </>
   )
