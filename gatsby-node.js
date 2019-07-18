6f9938adc7ea633f9eff3c9ea5d005841c99a01d
Neil Berg <neil.berg14@gmail.com>
2019-07-18 00:05:21 -0700

    initial blog rendering

--- gatsby-node.js
+++ gatsby-node.js
@@ -17,11 +17,19 @@ exports.createPages = async ({ actions, graphql }) => {
           gallery
         }
       }
+      allContentfulBlogPost {
+        nodes {
+          slug
+          title
+        }
+      }
     }
   `)
 
   const exhibitions = data.allContentfulExhibition.nodes
+  const posts = data.allContentfulBlogPost.nodes
 
+  // Create pages for each exhibition
   exhibitions.forEach((exhibition, index) => {
     const previous =
       index === exhibitions.length - 1 ? null : exhibitions[index + 1]
@@ -38,6 +46,23 @@ exports.createPages = async ({ actions, graphql }) => {
         date: exhibition.date,
         gallery: exhibition.gallery,
         pressRelease: exhibition.pressRelease.file.url,
+        previous,
+        next,
+      },
+    })
+  })
+
+  // Create pages for each blog post
+  posts.forEach((post, index) => {
+    const previous = index === posts.length - 1 ? null : posts[index + 1]
+    const next = index === 0 ? null : posts[index - 1]
+
+    actions.createPage({
+      path: `/thoughts/${post.slug}`,
+      component: path.resolve(`./src/templates/post-template.js`),
+      context: {
+        slug: post.slug,
+        title: post.title,
         previous,
         next,
       },
